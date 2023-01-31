28b96657d2cb7149448120ae1cb7e1ecac8783f2
zongbeen <hjb825@naver.com>
2023-01-31 19:19:05 +0900

    thymeleafEx

---
+++ back-end/src/main/java/com/shoppingMall/controller/ThymeleafExController.java
@@ -0,0 +1,20 @@
+package com.shoppingMall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//클라이언트의 요청에 대해서 어떤 컨트롤러가 처리할지 매핑하는 어노테이션
+//"/thymeleaf"경로로 오는 요청을 controller가 처리
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+    @GetMapping(value = "/ex01")
+    public String thymeleafExample01(Model model) {
+        //model 객체를 이용해 뷰에 전달한 데이터를 key, value 구조로 넣어준다.
+        model.addAttribute("data", "This is Thymeleaf example.");
+        //templates 폴더를 기준으로 뷰의 위치와 이름(thymeleafEx01.html)을 반환
+        return "thymeleafEx/thymeleafEx01";
+    }
+}
