4015a4eecca8e1a04dbd269f33fa0fce4c308f13
zongbeen <hjb825@naver.com>
2023-02-01 00:32:02 +0900

    Thymeleaf

--- back-end/src/main/java/com/shoppingMall/ShoppingMallApplication.java
+++ back-end/src/main/java/com/shoppingMall/ShoppingMallApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingMallApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShoppingMallApplication.class, args);
 	}
 
--- back-end/src/main/java/com/shoppingMall/controller/ThymeleafExController.java
+++ back-end/src/main/java/com/shoppingMall/controller/ThymeleafExController.java
@@ -1,14 +1,17 @@
 package com.shoppingMall.controller;
 
+import com.shoppingMall.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
 //클라이언트의 요청에 대해서 어떤 컨트롤러가 처리할지 매핑하는 어노테이션
 //"/thymeleaf"경로로 오는 요청을 controller가 처리
-@RequestMapping(value = "/thymeleaf")
+@RequestMapping(value = "/")
 public class ThymeleafExController {
     @GetMapping(value = "/ex01")
     public String thymeleafExample01(Model model) {
@@ -16,5 +19,16 @@ public class ThymeleafExController {
         model.addAttribute("data", "This is Thymeleaf example.");
         //templates 폴더를 기준으로 뷰의 위치와 이름(thymeleafEx01.html)을 반환
         return "thymeleafEx/thymeleafEx01";
+    }
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemName("테스트상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
     }
 }
